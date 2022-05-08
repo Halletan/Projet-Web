409e15c9287f8d985ac53899509101bd746e6929
Djounna <80339248+Djounna@users.noreply.github.com>
2022-05-08 20:36:31 +0200

    Début AircraftQueries

---
+++ SpaceAdventures/Application/Common/Queries/Aircrafts/AircraftsVm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceAdventures.Application.Common.Queries.Aircrafts
+{
+    public class AircraftsVm
+    {
+       
+            public IList<AircraftDto> AircraftsList { get; set; }
+        
+    }
+}
---
+++ SpaceAdventures/Application/Common/Queries/Aircrafts/GetAircraftsQuery.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using SpaceAdventures.Application.Common.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceAdventures.Application.Common.Queries.Aircrafts
+{
+    public class GetAircraftsQuery : IRequest<AircraftsVm>
+    {
+    }
+
+    public class GetAircraftsQueryHandler : IRequestHandler<GetAircraftsQuery, AircraftsVm>
+        {
+            private readonly IAircraftService _aircraftService;
+
+            public GetAircraftsQueryHandler(IAircraftService aircraftService)
+            {
+                _aircraftService = aircraftService;
+            }
+
+            public async Task<AircraftsVm> Handle(GetAircraftsQuery request, CancellationToken cancellationToken)
+            {
+                return await _aircraftService.GetAllAircrafts(cancellationToken);
+            }
+        }
+    
+}
