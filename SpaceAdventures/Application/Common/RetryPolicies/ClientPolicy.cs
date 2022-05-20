3c9da822ba0d792d1bbb4ffdcd031549cfe90611
hchadli <h.chadli@hotmail.be>
2022-05-21 00:42:12 +0200

    Circuit Breaker Config

--- SpaceAdventures/Application/Common/RetryPolicies/ClientPolicy.cs
+++ SpaceAdventures/Application/Common/RetryPolicies/ClientPolicy.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Polly;
+using Polly.CircuitBreaker;
 using Polly.Retry;
 
 namespace SpaceAdventures.Application.Common.RetryPolicies
@@ -13,21 +14,36 @@ namespace SpaceAdventures.Application.Common.RetryPolicies
         public AsyncRetryPolicy<HttpResponseMessage> ImmediateHttpRetry { get; }
         public AsyncRetryPolicy<HttpResponseMessage> LinearHttpRetry { get; }
         public AsyncRetryPolicy<HttpResponseMessage> ExponentialHttpRetry { get; }
+        public AsyncCircuitBreakerPolicy<HttpResponseMessage> CircuitBreakerRetry { get; }  
 
         public ClientPolicy()
         {
             // As indicated by his name, this will immediately launch retries after each failure
 
             ImmediateHttpRetry =
-                Policy.HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode).RetryAsync(5);
+                Policy
+                    .HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode)
+                    .RetryAsync(5);
 
             // Here we wait a bit and we retry again
-            LinearHttpRetry = Policy.HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode)
+            LinearHttpRetry = Policy
+                .HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode)
                 .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(3));
 
             // Exponential BackOff, It will increase the time after each failure, we use it for real scenarios
-            ExponentialHttpRetry = Policy.HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode)
+            ExponentialHttpRetry = Policy
+                .HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode)
                 .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+
+            // Circuit Breaker : When a problem is detected the circuit breaker moves to the open state,
+            // blocking all requests for specified period.
+            // After that period elapses the circuit breaker moves to a half-open state where the first request is treated as a test request.
+            // If this request succeeds the circuit closes and normal operation resumes,
+            // but if it fails the circuit moves back to open and remains there for a specified period before once again moving to half-open.
+
+            CircuitBreakerRetry = Policy
+                .HandleResult<HttpResponseMessage>(res => !res.IsSuccessStatusCode)
+                .CircuitBreakerAsync(5, TimeSpan.FromSeconds(30));
 
         }
     }
