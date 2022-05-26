465658e2dd8b0eb1c603cfc3346d6f654dff177c
hchadli <h.chadli@hotmail.be>
2022-05-27 01:11:01 +0200

    Update MappingExtensions.cs

--- SpaceAdventures/Application/Common/Mappings/MappingExtensions.cs
+++ SpaceAdventures/Application/Common/Mappings/MappingExtensions.cs
@@ -6,8 +6,8 @@ public static class MappingExtensions
 {
     // Extension method to use in order to convert the consumed list into a paginated list 
     public static Task<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(
-        this IQueryable<TDestination> queryable, int pageNumber, int pageSige)
+        this IQueryable<TDestination> queryable, int pageNumber, int pageSize)
     {
-        return PaginatedList<TDestination>.CreateAsync(queryable, pageNumber, pageSige);
+        return PaginatedList<TDestination>.CreateAsync(queryable, pageNumber, pageSize);
     }
 }
