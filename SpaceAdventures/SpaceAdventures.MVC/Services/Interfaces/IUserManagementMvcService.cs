507eb49c544f3f17128dfaf75a8173f8c24d1888
Djounna <80339248+Djounna@users.noreply.github.com>
2022-05-31 10:11:00 +0200

    clean mvc

--- SpaceAdventures/SpaceAdventures.MVC/Services/Interfaces/IUserManagementMvcService.cs
+++ SpaceAdventures/SpaceAdventures.MVC/Services/Interfaces/IUserManagementMvcService.cs
@@ -5,8 +5,5 @@ namespace SpaceAdventures.MVC.Services.Interfaces
     public interface IUserManagementMvcService
     {
         Task<User> CreateUser(string token, UserInput user);
-        Task<string> CreateUserOnAuth0(string token, UserInput user);
-        Task<User> CreateUserInDb(string? accessToken, UserInput user);
-        Task<bool> UserExistsInDb(string? accessToken, UserInput user);
     }
 }
--- SpaceAdventures/SpaceAdventures.MVC/Services/UserManagementMvcService.cs
+++ SpaceAdventures/SpaceAdventures.MVC/Services/UserManagementMvcService.cs
@@ -23,43 +23,7 @@ namespace SpaceAdventures.MVC.Services
            
         }
 
-        public async Task<User> CreateUser( string accessToken, UserInput user)
-        {      
-            string result = await CreateUserOnAuth0(accessToken, user);
-
-            return await CreateUserInDb(accessToken, user);               
-
-        }
-        public async Task<string> CreateUserOnAuth0(string token, UserInput user)
-        {
-            try
-            {
-                User u = new User
-                {
-                    Email = user.Email,
-                    Username = user.Username,                    
-                    Connection = "Username-Password-Authentication"                   
-                };
-
-                var postBody = JsonConvert.SerializeObject(u);
-                _httpClient.DefaultRequestHeaders.Authorization =
-                    new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-
-                var response = _httpClient.PostAsync(
-                    "https://dev-hlpjlsil.eu.auth0.com/api/v2/users", // a changer, faire passer le configuration[Auth0:domain]
-                    new StringContent(postBody, Encoding.UTF8, "application/json")).Result;
-                var responseString = response.Content.ReadAsStringAsync().Result;
-
-                return responseString;
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-        }
-    
-
-        public async Task<User> CreateUserInDb(string? accessToken, UserInput user)
+        public async Task<User> CreateUser(string? accessToken, UserInput user)
         {
             var postBody = JsonConvert.SerializeObject(user);
             _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
@@ -70,24 +34,6 @@ namespace SpaceAdventures.MVC.Services
             var content = await response.Content.ReadAsStringAsync();
             return JsonConvert.DeserializeObject<User>(content);
 
-        }
-
-        public async Task<bool> UserExistsInDb(string? accessToken, UserInput user)
-        {
-            throw new NotImplementedException();
-
-            // UserExists to implement in SpaceAdventures API Controllers (already implemented in Services)
-
-
-            //string email = user.Email;
-
-            //_httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
-            //var response = await _httpClient.GetAsync("https://localhost:7195/api/v1.0/Users/UserExists/"+email);
-
-            //if (!response.IsSuccessStatusCode) throw new Exception("Cannot retrieve data");
-
-            //var content = await response.Content.ReadAsStringAsync();
-            //return JsonConvert.DeserializeObject<bool>(content);
         }
 
     }
