e4d716ccee0b038d58199a6c34e878544dfdbf06
Djounna <80339248+Djounna@users.noreply.github.com>
2022-05-31 22:31:41 +0200

    FormCreateUser

--- SpaceAdventures/SpaceAdventures.MVC/Controllers/UserManagementController.cs
+++ SpaceAdventures/SpaceAdventures.MVC/Controllers/UserManagementController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using SpaceAdventures.MVC.Models;
 using SpaceAdventures.MVC.Services.Interfaces;
@@ -17,7 +18,7 @@ namespace SpaceAdventures.MVC.Controllers
             _userManagementMvcService = userManagementMvcService;
         }
 
-
+        #region Get List Users
         public async Task<IActionResult> GetAllUsers()
         {
             Users result = await _userManagementMvcService.GetAllUsers(await HttpContext.GetTokenAsync("access_token"));
@@ -32,14 +33,25 @@ namespace SpaceAdventures.MVC.Controllers
                 vm.Role = userRole.name;
                 lst.Add(vm);
             }
-
+            
 
             return View(lst);
         }
+        #endregion
 
         #region CreateUser
         public IActionResult CreateUser()
         {
+            IEnumerable<SelectListItem> listRole = new List<SelectListItem>()
+            {
+                new SelectListItem(){Value="1", Text="Admin"},
+                new SelectListItem(){Value="2", Text="Admina"},
+                new SelectListItem(){Value="3", Text="Owner"},
+                new SelectListItem(){Value="4", Text="User"}
+            };
+            ViewBag.listRole = listRole;
+
+
             UserInput user = new();
             return View(user);
         }
@@ -48,20 +60,10 @@ namespace SpaceAdventures.MVC.Controllers
         [ActionName(nameof(CreateUser))]
         public async Task<IActionResult> PostUser(UserInput userInput)
         {
-            // if success
-            // if user does not exist in DB
-            // Create user in Auth0 (Call API Auth0)
-            // Create user in DB (call API)
-            // Give userRole set by admin
-            // Give customer role in Auth0
-            //Else  (modified user that already exist) or send error message ? 
-            //Else Access denied
-
             string accessToken = await HttpContext.GetTokenAsync("access_token");
 
             if (ModelState.IsValid)
-            {
-                
+            {               
                 User userCreated=  await _userManagementMvcService.CreateUser(accessToken, userInput);
 
                 return View(userCreated); // Sera une RedirectToAction vers GetUserDetails(user)
