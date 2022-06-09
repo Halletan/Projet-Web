64c17cdf89133c4275cb19afad10fe8a40e64bb1
hchadli <h.chadli@hotmail.be>
2022-06-09 22:22:06 +0200

    View Role Updated

--- SpaceAdventures/SpaceAdventures.MVC/Configurations/ServicesConfig.cs
+++ SpaceAdventures/SpaceAdventures.MVC/Configurations/ServicesConfig.cs
@@ -8,6 +8,7 @@ public static class ServicesConfig
     public static IServiceCollection AddServiceCollection(this IServiceCollection services)
     {
         services.AddHttpContextAccessor();
+
         services.AddDistributedMemoryCache();
         services.AddSession();
 
