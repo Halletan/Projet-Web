5484ffe2d89cc12cfee8cdcf61ce1e5d6bbbab76
hchadli <h.chadli@hotmail.be>
2022-06-14 14:04:22 +0200

    Tiny Change on ApiVersioning

--- SpaceAdventures/SpaceAdventures.API/Configurations/VersionApiExplorerConfig.cs
+++ SpaceAdventures/SpaceAdventures.API/Configurations/VersionApiExplorerConfig.cs
@@ -9,8 +9,7 @@ public static class VersionApiExplorerConfig
             // Instruct Swagger/ApiVersionExplorer how it will handle this concept of different versions that we have.
 
             setup.GroupNameFormat = "'v'VVV"; // How to read this version 
-            setup.SubstituteApiVersionInUrl =
-                true; // When we actually run our api we want that the version should be substituted auto. in our URLs
+            setup.SubstituteApiVersionInUrl = true; // When we actually run our api we want that the version should be substituted auto. in our URLs
         });
 
         return services;
